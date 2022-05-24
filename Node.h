4fd583461f5cc2b9a1e36162cd49f2afd5571b01
Dambi <matan.wink@gmail.com>
2022-05-24 10:56:23 +0300

    Started working on Iterator.

--- Node.h
+++ Node.h
@@ -1,20 +1,52 @@
+//TODO: is "previous" needed?
 #ifndef HW3_NODE_H
 #define HW3_NODE_H
 
 template<class T> class Node{
 
 public:
-    Node();
+    explicit Node();
     Node(const Node&);
     ~Node() = default; //TODO: default or do i need to somehow delete the content?
 
-    bool connect_next(Node* other);
-    bool connect_previous(Node* other);
+    void connect_next(const Node* other);
+    Node* get_next();
+    T get_content();
+//    void connect_previous(const Node* other);
 
 private:
     T content;
-    Node next;
-    Node previous;
+    Node* next; //link to next node
+//    Node previous;
 };
+
+template<class T>
+
+//----------------------implementation---------------------//
+
+void Node<T>::connect_next(const Node* other){
+    next = other;
+}
+
+//template<class T>
+//void Node<T>::connect_previous(const Node *other) {
+//    previous = other;
+//}
+
+template<class T>
+Node<T>::Node() : content(), next(nullptr)  { //TODO: memory allocation on heap instead of stack for new nodes/for new content.
+
+}
+
+template<class T>
+T Node<T>::get_content() {
+    return &content;
+}
+
+template<class T>
+Node<T> *Node<T>::get_next() {
+    return next;
+}
+
 
 #endif //HW3_NODE_H
--- Queue.h
+++ Queue.h
@@ -2,7 +2,8 @@
 
 #ifndef HW3_QUEUE_H
 #define HW3_QUEUE_H
-const int START_SIZE = 100;
+#include "Node.h"
+const int START_nodes_amount = 100;
 const int EXPAND_RATE = 4;
 
 template<class T> class Queue{
@@ -12,7 +13,12 @@ public:
     Queue& operator=(const Queue&); // '=' operator
     ~Queue(); //deconstructor
 
-    void pushback(T item);
+    void pushBack(T item);
+    T& front();
+//    const T& front(); //TODO: syntax. why cant i make a const version of this function like in the slides?
+    void popFront();
+    int size();
+
 
     //-------------------Iterator------------------------// 
     class Iterator;  //TODO: syntax for declaring one class inside another?
@@ -21,38 +27,118 @@ public:
 
 
 private:
-    T* first;
-    T* last;
-    int size;
+    Node<T>* first;
+    Node<T>* last;
+    int nodes_amount;
 };
 
-template<class T> class Queue<T>::Iterator {
+//---------------------------------Iterator class------------------------------------------//
+template<class T> class Queue<T>::Iterator { //TODO: needs to be 'Iterator<T>' or 'Iterator'?
     //TODO: finish Iterator using slides from lecture about operators. or from tutorial.
 public:
-    Iterator(); //TODO: finish constructor
+    explicit Iterator(Queue<T> *queue); //TODO: finish constructor
     Iterator(const Iterator&) = default;
     ~Iterator() = default;
+
+    //-----------------------------Iterator operators-------------------------------------//
+    bool operator!=(const Iterator &other);
+    T operator*();
+    void operator++();
 
     Iterator begin() const;
     Iterator end() const;
 
 private:
     const Queue<T>* queue;
+    Node<T>* current;
 
-    friend class Queue<T>;
+    friend class Queue<T>; //TODO: make sure friend is needed for internal class.
 };
 
-//-------------------implementation of template functions----------------------------//
+
+//-------------------implementation of template Iterator functions----------------------------//
+
+
 template<class T>
-Queue<T>::~Queue() {
+bool Queue<T>::Iterator::operator!=(const Queue<T>::Iterator &other) {
+    return current != other.current;
 }
 
 template<class T>
-Queue<T>::Queue() : size(0), first(), last()
-{}
+T Queue<T>::Iterator::operator*() {
+    return *current; //supposed to return current, not pointer to current.
+}
 
 template<class T>
-void Queue<T>::pushback(T item) {
+void Queue<T>::Iterator::operator++() {
+    current = (current->get_next());
+}
+
+template<class T>
+typename Queue<T>::Iterator Queue<T>::Iterator::begin() const {
+    Iterator start(queue);
+    start.current = *queue->first;
+    return start;
+}
+
+template<class T>
+typename Queue<T>::Iterator Queue<T>::Iterator::end() const {
+    Iterator temp(queue);
+//    temp.current = *queue->last;
+    temp.current = nullptr;
+    return temp;
+}
+
+template<class T>
+Queue<T>::Iterator::Iterator(Queue<T> *queue) : queue(queue) { //this.queue = address of input queue.
+
+}
+
+
+//-------------------implementation of template Queue functions----------------------------//
+template<class T>
+Queue<T>::~Queue() {  //TODO: code copied from HW1. validity needs to be checked.
+    Node<T> head = first;
+    Node<T> temp;
+    while (head->next) {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+    free(head);
+}
+
+template<class T>
+Queue<T>::Queue() : nodes_amount(0), first(new Node<T>), last(first){
+
+} //TODO: syntax for assigning memory on heap in constructors.
+
+
+template<class T>
+void Queue<T>::pushBack(T item) {
+    Node<T> addition = new Node<T>;
+    last->connect_next(addition);
+    last = *addition;
+    nodes_amount++;
+}
+
+template<class T>
+T& Queue<T>::front() {
+    if (size() > 0)
+    {
+        return &first->get_content();
+    }
+    return nullptr; //no nodes so no content
+}
+
+template<class T>
+void Queue<T>::popFront() {
+
+}
+
+template<class T>
+int Queue<T>::size() {
+    return 0;
 }
 
 
